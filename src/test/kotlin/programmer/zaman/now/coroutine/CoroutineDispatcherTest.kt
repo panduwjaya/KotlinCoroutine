fd047e307ca2635ed5093cf680edd9ff89755b06
panduwjaya <wijayapandu388@gmail.com>
2022-08-02 13:44:20 +0700

    menyelesaikan catatan Non cancellabel context

--- src/test/kotlin/programmer/zaman/now/coroutine/CoroutineDispatcherTest.kt
+++ src/test/kotlin/programmer/zaman/now/coroutine/CoroutineDispatcherTest.kt
@@ -173,7 +173,53 @@ class CoroutineDispatcherTest {
     }
 
     /*
+    ================ Non Cancellable Context =================
 
+    Check isActive di Finally
+    Sebelumnya kita tahu bahwa setelah coroutine di cancel, dan jika kita ingin melakukan sesuatu, kita bisa menggunakan block try-finally
+    Namun dalam block finally, kita tidak bisa menggunakan suspend method yang mengecek isActive, karena otomatis akan bernilai false, dan otomatis batal
+
+    * Kode : Check isActive di Finally
+    @Test
+    fun testCancelFinally(){
+    runBlocking{
+        val job=GlobalScope.Launch{
+             try{
+                 println("Hello$(Date())")
+                 delay(2008)
+                 println("World$(Date())")
+             }finally{
+                 println(isActive)
+                 delay(1000)
+                 println("Finally")
+             }
+        }
+        job.cancelAndJoin()
+    }
+
+    Non Cancellable Context
+    Jika kita butuh memanggil suspend function yang mengecek isActive di block finally, dan berharap tidak dibatalkan eksekusinya, maka kita bisa menggunakan NonCancellable
+    NonCancellable adalah coroutine context yang mengoverride nilai-nilai cancellable sehingga seakan-akan coroutine tersebut tidak di batalkan
+
+    * Kode : Non Cancellable Context
+    @Test
+    fun testNonCancellable(){
+    runBlocking{
+        val job GlobalScope.Launch{
+            try{
+                 println("Hello${Date()}")
+                 delay(2008)
+                println("World${Date()}")
+            }finally{
+                withContext(NonCancellable){
+                     println(isActive)
+                     delay(1000)
+                     println("Finally")
+                }
+            }
+        }
+        job.cancelAndJoin()
+    }
      */
 
     @Test
