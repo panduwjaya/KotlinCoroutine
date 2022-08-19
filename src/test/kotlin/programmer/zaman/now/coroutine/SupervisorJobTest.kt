4dbd521660ae6bd2b7ff9c031a140962c067ac88
panduwjaya <wijayapandu388@gmail.com>
2022-08-19 23:26:26 +0700

    Menyelesaikan SupervisorScope pada ExceptionHandler

--- src/test/kotlin/programmer/zaman/now/coroutine/SupervisorJobTest.kt
+++ src/test/kotlin/programmer/zaman/now/coroutine/SupervisorJobTest.kt
@@ -171,6 +171,22 @@ class SupervisorJobTest {
          */
     }
 
+    /*
+    ========== Exception Handler di Job vs Supervisor Job ==========
+
+    Exception Handler di Job dan Supervisor Job
+    Exception handler di Job ataupun di Supervisor Job secara default akan di propagate ke parent nya,Artinya ketika terjadi Exception maka permasalahan tersebut tidak diselesaikan pada child nya
+    Namun akan diselesaikan pada parent nya dengan cara di propagate Exception ke parentnya agar permasalahn tsb dapat diselesaikan pada parent
+    Artinya jika kita membuat CoroutineExceptionHandler, kita harus membuatnya di parent, tidak bisa di coroutine child nya.
+    Jika kita menambahkan exception handler di coroutine child nya, maka itu tidak akan pernah digunakan
+
+    catatan:
+    solusi agar exceptionHandler dapat digunakan pada child menggunakan Exception Handler dengan supervisorScope
+    Namun sebenarnya ExceptionHandler hanya dapat digunakan pada parent bukan child,pada supervisorScope hanya bertugas merekayasa menggantikan parent pada child
+     */
+
+    // Contoh kode exceptionHandler yg salah
+    // Alasan kode ini salah karena menempatkan ExceptionHandler pada child bukan pada parent sedangkan ExceptionHandler hanya bisa dieksekusi pada parent
     @Test
     fun testJobExceptionHandler() {
         val exceptionHandler = CoroutineExceptionHandler { context, throwable ->
@@ -182,7 +198,7 @@ class SupervisorJobTest {
 
         runBlocking {
             val job = scope.launch {
-                launch(exceptionHandler) { // not used
+                launch(exceptionHandler) { // not used,artinya ExceptionHandler ini tidak digunakan atau sia sia
                     println("Job Child")
                     throw IllegalArgumentException("Child Error")
                 }
@@ -190,7 +206,55 @@ class SupervisorJobTest {
 
             job.join()
         }
+        /*
+        output:
+        Job Child
+        Exception in thread "pool-1-thread-2 @coroutine#3" java.lang.IllegalArgumentException: Child Error
+            at programmer.zaman.now.coroutine.SupervisorJobTest$testJobExceptionHandler$1$job$1$1.invokeSuspend(SupervisorJobTest.kt:199)
+            at kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith(ContinuationImpl.kt:33)
+            at kotlinx.coroutines.DispatchedTask.run(DispatchedTask.kt:106)
+         */
     }
+
+    /*
+    Exception Handler dengan supervisorScope
+    Salah satu cara agar exception handler bisa dilakukan di coroutine child adalah dengan menggunakan supervisorScope
+    Saat menggunakan supervisorScope, maka exception bisa di gunakan di parent coroutine di supervisorScope, atau sebenarnya coroutine child di scope yang ada diatas nya
+    Tapi ingan jika terjadi error di child nya coroutine yang ada di supervisorScope, maka tetap akan di propagate ke parent coroutine di cupervisorScope
+
+    catatan:
+    solusi agar exceptionHandler dapat digunakan pada child menggunakan Exception Handler dengan supervisorScope
+    Namun sebenarnya ExceptionHandler hanya dapat digunakan pada parent bukan child,pada supervisorScope hanya bertugas merekayasa menggantikan parent pada child
+
+    example:
+    cara peletakan exceptionHandler tidak sembarangan
+    Artinya untuk meletakan exceptionHandler harus berada tepat diatas child yang akan menggunakan exceptionHandler
+    Tidak meletakan exceptionHandler tepat pada child yg akan dieksekusi
+
+    contoh exceptionHandler yg not used:
+    runBlocking {
+            val job = scope.launch {
+                supervisorScope {
+                    launch(exceptionHandler) {
+                        launch(exceptionHandler) { // not used
+                            println("Job Child")
+                            throw IllegalArgumentException("Child Error")
+                        }
+
+
+    contoh exceptionHandler yg used:
+    runBlocking {
+            val job = scope.launch {
+                supervisorScope {
+                    launch(exceptionHandler) {
+                        launch { // used
+                            println("Job Child")
+                            throw IllegalArgumentException("Child Error")
+                        }
+
+    Dengan adanya supervisorScope maka exceptionHandler dapat digunakan pada scope child tanpa peletakan sembarangan
+    Yaitu peletekan harus diletakan pada tepat diatas child yang akan terkena exception bukan pada child yang terkena exception karena itu tidak berguna
+     */
 
     @Test
     fun testSupervisorJobExceptionHandler() {
@@ -204,8 +268,8 @@ class SupervisorJobTest {
         runBlocking {
             val job = scope.launch {
                 supervisorScope {
-                    launch(exceptionHandler) {
-                        launch(exceptionHandler) { // not used
+                    launch(exceptionHandler) { // berperan sebagai pengganti parent pada child,padahal bukan parent sebenarnya
+                        launch(exceptionHandler) { // exceptionHandler not used,artinya yg digunakan hanya exceptionHandler yg berada tepat diatas launch
                             println("Job Child")
                             throw IllegalArgumentException("Child Error")
                         }
@@ -215,5 +279,13 @@ class SupervisorJobTest {
 
             job.join()
         }
+        /*
+        output:
+        Job Child
+        Error Child Error
+
+        catatan:
+        Dengan adanya supervisorScope pada child kita dapat menggunakan ExceptionHandler pada child dan mampu menghandle jika terjadi exception menggunakan ExceptionHandler di child
+         */
     }
 }
