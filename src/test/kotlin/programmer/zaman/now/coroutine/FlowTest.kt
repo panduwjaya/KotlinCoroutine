c369432621e88e79ce86313d17b3a65deec6ef75
panduwjaya <wijayapandu388@gmail.com>
2022-08-20 22:16:30 +0700

    menyelesaikan Asyncronous flow

--- src/test/kotlin/programmer/zaman/now/coroutine/FlowTest.kt
+++ src/test/kotlin/programmer/zaman/now/coroutine/FlowTest.kt
@@ -6,22 +6,63 @@ import org.junit.jupiter.api.Test
 import java.util.*
 
 class FlowTest {
+    /*
+    ======= Asynchronous Flow ========
 
+    * Asynchronous Flow
+    Sampai saat ini kita hanya membahas tentang coroutine yang tidak mengembalikan value (launch) dan mengembalikan satu value (async)
+    bagaimana jika kita butuh sebuah coroutine yang mengembalikan data berkali-kali seperti layaknya collection? Kotlin mendukung hal tersebut dengan nama Flow.
+    Flow mirip dengan sequence di Kotlin Collection, yang membedakan adalah flow berjalan sebagai coroutine dan kita bisa menggunakan suspend function di flow.
+    Flow adalah collection cold atau lazy, artinya jika tidak diminta datanya, flow tidak akan berjalan (kode nya tidak akan dieksekusi)
+    Dikarenakan flow adalah lazy maka diperlukan sebuah kode untuk memanggilnya agar mau berjalan yaitu menggunakan collect()
+
+    * Membuat Flow
+    -Untuk membuat flow, kita bisa menggunakan function flow()
+    -Di dalam flow untuk mengirim data ke flow kita bisa menggunakan function emit()
+     fungsi dari emit adalah untuk mengirim ke collect(),saat emit mendapat data maka secara langsung akan diterima di collect()
+    -Untuk mengakses data yang ada di flow, kita bisa menggunakan function collect()
+     Dikarenakan flow adalah lazy maka diperlukan sebuah kode untuk memanggilnya agar mau berjalan yaitu menggunakan collect(),apabila tidak dipanggil maka kode tsb tidak akan dieksekusi
+     */
     @Test
     fun testFlow() {
         val flow1: Flow<Int> = flow {
             println("Start flow")
-            repeat(100) {
+            repeat(5) {
                 println("Emit $it")
                 emit(it)
             }
         }
 
         runBlocking {
+            // dikarenakan collect merupakan suspend function maka untuk memanggilnya perlu didalam runBlocking
+            // jika flow tidak dipanggil menggunakan collect maka kode tersebut tidak akan di eksekusi,karena sifatnya yg lazy
             flow1.collect {
                 println("Receive $it")
             }
         }
+        /*
+        output:
+        Start flow
+        Emit 0
+        Receive 0
+        Emit 1
+        Receive 1
+        Emit 2
+        Receive 2
+        Emit 3
+        Receive 3
+        Emit 4
+        Receive 4
+        Emit 5
+        Receive 5
+
+        catatan:
+        Sistem kerja pada flow mirip dengan menggunakan squence,yaitu dikerjakan secara berurut
+        Tidak menyelesaikan salah satu terlebih dahulu sampai selesai,namun dikerjakan secara bersama dan berurutan
+        Saat emit() mendapat data maka secara langsung data tersebut diterima di collect()
+        Namun jika collect tidak panggil maka data tersebut tidak akan dieksekusi karena collect() bersifat lazy
+        Maka dari itu hasil dari output pada flowTest terlihat squence atau berurutan,karena setiap proses dikerjakan secara langsung tanpa saling tunggu
+         */
     }
 
     suspend fun numberFlow(): Flow<Int> = flow {
